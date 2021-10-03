41978119a6377a5f980f7365e1c84614f9212fbd
aref19 <arefobaid6@gmail.com>
2021-10-03 20:31:04 +0200

    Alone

---
+++ hauptseiet1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf8">
+
+    <body>
+        <p>yesss</p>
+<?php
+echo "jaaa";
+?>
+    </body>
+</head>
+
+
+</html>
---
+++ neu 1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>ja</p>
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html>
