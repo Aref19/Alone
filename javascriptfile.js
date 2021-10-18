94f911d2440c28318a5a26e464cdd0b79b1755e2
Aref <arefobaid6@gmail.com>
2021-10-18 22:05:32 +0200

    we!

---
+++ javascriptfile.js
@@ -0,0 +1,10 @@
+
+var e = document.getElementById("cars");
+var value = e.options[e.selectedIndex].value;
+var text = e.options[e.selectedIndex].text;
+var selectedItem = function () {
+    
+    return carstyp
+}
+console.log(value);
+
